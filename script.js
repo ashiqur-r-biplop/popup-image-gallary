8c11c04ac7948518fd0c337586728fcfe4b606ce
ashiqur-r-biplop <biplops22@gmail.com>
2024-09-02 05:33:14 +0600

    added final code

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-// popup image  function
+function openImage(element) {
+  const popupContainer = document.querySelector(".popup-container");
+  const popupImage = document.getElementById("popupimage");
+  const animation = document.getElementById("animation");
+
+  popupImage.src = element.src;
+
+  popupContainer.style.display = "flex";
+
+  // Trigger the scale animation
+  setTimeout(() => {
+    animation.style.transform = "scale(1)";
+  }, 0);
+}
+
+function closePopup() {
+  const popupContainer = document.querySelector(".popup-container");
+  const animation = document.getElementById("animation");
+
+  animation.style.transform = "scale(0)";
+
+  setTimeout(() => {
+    popupContainer.style.display = "none";
+  }, 300);
+}
+
+const images = document.querySelectorAll(".image-box img");
+images.forEach((image) => {
+  image.addEventListener("click", function () {
+    openImage(this);
+  });
+});
+
+const closeIcon = document.querySelector(".close-icon");
+closeIcon.addEventListener("click", closePopup);
+
+const popupContainer = document.querySelector(".popup-container");
+popupContainer.addEventListener("click", function (event) {
+  if (event.target === popupContainer) {
+    closePopup();
+  }
+});
