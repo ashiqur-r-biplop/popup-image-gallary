b93394155c17dc98b7e6ad3b426aa05cad89e1e0
ashiqur-r-biplop <biplops22@gmail.com>
2024-09-02 16:18:26 +0600

    added

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ function openImage(element) {
   const popupContainer = document.querySelector(".popup-container");
   const popupImage = document.getElementById("popupimage");
   const animation = document.getElementById("animation");
+  const imageGallery = document.querySelector(".image-gallery");
 
   popupImage.src = element.src;
 
   popupContainer.style.display = "flex";
+  document.body.classList.add("no-scroll");
+  imageGallery.classList.add("top"); // Move the gallery to the top
 
   // Trigger the scale animation
   setTimeout(() => {
@@ -16,11 +19,14 @@ function openImage(element) {
 function closePopup() {
   const popupContainer = document.querySelector(".popup-container");
   const animation = document.getElementById("animation");
+  const imageGallery = document.querySelector(".image-gallery");
 
   animation.style.transform = "scale(0)";
 
   setTimeout(() => {
     popupContainer.style.display = "none";
+    document.body.classList.remove("no-scroll");
+    imageGallery.classList.remove("top"); // Move the gallery back to its original position
   }, 300);
 }
 
